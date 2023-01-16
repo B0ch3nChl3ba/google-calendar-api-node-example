d3cadf41663d42491f5d640805bd76578c15e27e
Bochen <--->
2023-01-16 16:29:06 +0100

    added token refresh and multi-user functionality

--- main.js
+++ main.js
@@ -1,38 +1,92 @@
-require("dotenv").config()
+require('dotenv').config()
 const express = require('express')
+const basicAuth = require('express-basic-auth')
+const JSONdb = require('simple-json-db')
 const { google } = require('googleapis')
 
 const app = express()
 const port = 8080
+const db = new JSONdb(__dirname + '/db.json')
 
-const oauth2Client = new google.auth.OAuth2(
+const googleAuthApi = new google.auth.OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
     process.env.REDIRECT_URL,
-);
+)
 
 const scopes = [
     'https://www.googleapis.com/auth/calendar'
-];
+]
 
-app.get('/', (req, res) => {
-    const url = oauth2Client.generateAuthUrl({
-        // 'online' (default) or 'offline' (gets refresh_token)
+app.use(basicAuth({
+    challenge: true,
+    authorizer: (u, p) => {
+        if (db.has(u)) {
+            return db.get(u).password === p
+        } else {
+            db.set(u, { password: p })
+            return true
+        }
+    }
+}))
+
+app.use((req, res, next) => {
+    req.user = { name: req.auth.user, ...db.get(req.auth.user) }
+    next()
+})
+
+app.get('/', async (req, res) => {
+    if (req.user.tokens) {
+        const auth = new google.auth.OAuth2()
+        auth.setCredentials(req.user.tokens)
+
+        if (Date.now() > req.user.tokens.expiry_date) {
+            const res = await auth.refreshAccessToken()
+            const newTokens = res.credentials
+
+            db.set(req.user.name, {
+                ...req.user,
+                tokens: newTokens
+            })
+        }
+
+        const calendarList = await google.calendar({ version: 'v3', auth }).calendarList.list()
+
+        res.send(`
+            <pre>${JSON.stringify(calendarList.data, null, 2)}</pre>
+            <br>
+            <a href='/logout'>logout from ggl</a>
+        `)
+    } else {
+        res.send('<a href="/login">login to ggl</a>')
+    }
+})
+
+app.get('/login', (req, res) => {
+    const url = googleAuthApi.generateAuthUrl({
         access_type: 'offline',
-        // If you only need one scope you can pass it as a string
         scope: scopes,
-        prompt: "select_account"
-    });
+        prompt: 'select_account'
+    })
 
     res.redirect(url)
 })
 
-app.get('/redirect', async (req, res) => {
-    const { tokens } = await oauth2Client.getToken(req.query.code)
-    oauth2Client.setCredentials(tokens);
+app.get('/logout', (req, res) => {
+    req.user.tokens = undefined
+    db.set(req.user.name, req.user)
+    res.redirect('/')
+})
 
-    const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
-    res.json(await calendar.calendarList.list())
+app.get('/redirect', async (req, res) => {
+    const { tokens } = await googleAuthApi.getToken(req.query.code)
+
+    db.set(req.user.name, {
+        ...req.user,
+        tokens
+    })
+
+    res.redirect('/')
 })
 
 app.listen(port, () => {
